bab9791a3d7b04302d9f947ea89d744dc0b42d3d
Nick Cruz <nickcruz920@gmail.com>
2023-02-15 14:57:59 -0500

    Added addGroup() function

--- index.js
+++ index.js
@@ -30,3 +30,20 @@ input.addEventListener("keypress", function(event) {
 })
    
 }
+
+function addGroup() {
+  let mGroupText = document.querySelector(".mGroupText");
+  let mGroupPass = document.querySelector(".mGroupPass");
+  let mGroupNum = document.querySelector(".mGroupNum");
+  let mGroupOutput = document.getElementById("mGroupOutput");
+
+  let mGroup = [mGroupText.value, mGroupPass.value, mGroupNum.value];
+
+  mGroupOutput.innerHTML = `
+    <div class="mvContainer">
+    <h2 class="mvText"> Multiple Value Submissions</h2>
+    <h5 class="blueText">The Text Value is:${mGroup[0]}</h5> \<br>\
+    <h5 class="redText">The Password Value is:${mGroup[1]}</h5> \<br>\
+    <h5 class="greenText">The Number Value is:${mGroup[2]}</h5>  
+    </div>`;
+}
